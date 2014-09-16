573877b422a79048f377b88f165a38c327b0ff76
shitikovkirill <sh.kiruh@gmail.com>
2014-09-16 23:24:43 +0300

    I create a delete metod

---
+++ src/WorkWithFile.java
@@ -0,0 +1,25 @@
+import java.io.File;
+
+
+public class WorkWithFile {
+	// ------------ 		Удаление файла 		---------------------------
+	public void delete(File file){
+		
+		if(!file.exists())fail("Такого файла нет!");
+		if(!file.canWrite())fail("Файл защещен!");
+		if(file.isDirectory()){
+			String[] files = file.list();
+			if(files.length>0)fail("Каталог удалить нельзя потому, что каталог не пуст!");
+		}
+	 boolean cusses = file.delete();
+	 if(cusses)System.out.println("Файл удален");
+	}
+	
+	public void delete(String filename){
+		delete(new File(filename));	
+	}
+	
+	private void fail(String msg) throws  IllegalArgumentException {
+		throw new IllegalArgumentException(msg);
+	}
+}
