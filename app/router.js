549622e63852f565d993af8cfd2c76fa11cdbf65
Shitikov <sh.kiruh@gmail.com>
2018-01-31 17:42:03 +0200

    Revert changes

--- app/router.js
+++ app/router.js
@@ -1,21 +1,12 @@
-import Ember from 'ember';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
-  location: config.locationType
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('sightings', function() {
-    this.route('new');
-  });
-  this.route('sighting', function() {
-    this.route('edit');
-  });
-  this.route('cryptids');
-  this.route('cryptid');
-  this.route('witnesses');
-  this.route('witness');
 });
 
 export default Router;
